f867513bfdcdeaa4bb4252bb5609acfb8f8b3691
YevhenHryhorenko <116802765+YevhenHryhorenko@users.noreply.github.com>
2023-04-11 07:31:59 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
-const res = pow(3, 4);
+let num, degree;
+
+do {
+    num = parseFloat(prompt("Please enter the number:"));
+
+    if (isNaN(num)) {
+        alert("That's not a valid number. Please try again.");
+    }
+} while (isNaN(num));
+
+do {
+    degree = parseFloat(prompt("Please enter the power:"));
+
+    if (isNaN(degree)) {
+        alert("That's not a valid number. Please try again.");
+    }
+} while (isNaN(degree));
+
+const res = pow(num, degree);
 
 function pow(num, degree) {
-    return degree != 1 ? num * pow(num, degree - 1) : num
+    return degree == 1 ? num : num * pow(num, degree - 1);
 }
 
 document.write(`<h2>The number is raised to a power = ${res}</h2>`)
